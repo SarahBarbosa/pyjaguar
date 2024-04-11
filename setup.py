61b21be0e0ca4c840d096dc4a9af72933a317615
SarahBarbosa <sarah@fisica.ufc.br>
2024-04-11 16:44:16 -0300

    atualizando setup

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(name="pyjaguar", 
-      version="0.0.1", 
-      packages=find_packages(), 
-      install_requires=["numpy"])
+setup(
+    name="pyjaguar", 
+    version = "0.0.1",
+    description = "Biblioteca de análise/manipulação de dados ",
+    long_description = "README.md",
+    author = "Sarah G. A. Barbosa",
+    author_email = "sarahg.aroucha@gmail.com",
+    packages = ["pyjaguar"],
+    install_requires = ["numpy"], 
+    zip_safe = False,
+    )
